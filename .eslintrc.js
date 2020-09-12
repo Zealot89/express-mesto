ee450efba622edd3524ba1285154a4efd9e0932e
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-09-12 07:49:51 +0300

    fix: Проектная работа 12. Исправление замечаний.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,53 @@
 module.exports = {
+
   env: {
+
     browser: true,
+
     commonjs: true,
+
     es2021: true,
+
   },
+
   extends: [
-    'plugin:react/recommended',
+
     'airbnb',
+
   ],
+
   parserOptions: {
+
     ecmaFeatures: {
+
       jsx: true,
+
     },
+
     ecmaVersion: 12,
+
   },
+
   plugins: [
-    'react',
+
   ],
+
   rules: {
     'no-underscore-dangle': ['error', {
+
       allow: ['foo_', '_bar'],
+
       allowAfterThis: true,
+
       allowAfterSuper: true,
+
       allowAfterThisConstructor: true,
+
     }],
+
     'eol-last': ['error', 'never'],
+    'linebreak-style': 0,
+
   },
+
 };
--- routes/cards.js
+++ routes/cards.js
@@ -5,16 +5,14 @@ const cardsRouter = require('express').Router();
 const cards = path.join('./data/cards.json');
 
 cardsRouter.get('/cards', (req, res) => {
-    fs.readFile(cards, { encoding: 'utf8' }, (e, data) => {
-        const dataCards = JSON.parse(data);
-        if (e) {
-            res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
-            return
-        }
-        res.send(dataCards);
-    })
+  fs.readFile(cards, { encoding: 'utf8' }, (e, data) => {
+    const dataCards = JSON.parse(data);
+    if (e) {
+      res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+      return;
+    }
+    res.send(dataCards);
+  });
 });
-
-
 
 module.exports = cardsRouter;
