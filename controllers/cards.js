bb4f149c4b79e9a3f9b87c66d6906246fa85becc
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-09-25 19:39:02 +0300

    fix: Проектная работа 13. Исправление замечаний.

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,24 +5,36 @@ const Card = require('../models/card');
 
 const getCard = (req, res) => {
   Card.find({})
-    .then((data) => { res.status(200).send({ data }); })
+    .then((data) => {
+      res.status(200).send({ data });
+    })
     .catch((err) => {
       const ERROR_CODE = 404;
 
-      if (err.name === 'CastError') res.status(ERROR_CODE).send({ message: 'Запрашиваемый ресурс не найден' });
+      if (err.name === 'CastError') {
+        res
+          .status(ERROR_CODE)
+          .send({ message: 'Запрашиваемый ресурс не найден' });
+      }
     });
 };
 
 const addCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({
-    name, link, owner: req.user._id,
+    name,
+    link,
+    owner: req.user._id,
   })
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
-      res.status(400).send({ message: 'переданы некорректные данные в метод создания карточки' });
+      res
+        .status(400)
+        .send({
+          message: 'переданы некорректные данные в метод создания карточки',
+        });
     });
 };
 
@@ -31,9 +43,17 @@ const deleteCard = (req, res) => {
     .orFail(new Error('Not Found'))
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      const ERROR_CODE = 400;
+      const ERROR_CODE = 404;
 
-      if (err.name === 'CastError' || err.message === 'Not Found') { res.status(ERROR_CODE).send({ message: 'переданы некорректные данные в метод удаления карточки' }); } else { res.status(500).send({ message: 'Ошибка на сервере' }); }
+      if (err.name === 'CastError' || err.message === 'Not Found') {
+        res
+          .status(ERROR_CODE)
+          .send({
+            message: 'переданы некорректные данные в метод удаления карточки',
+          });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
     });
 };
 
--- controllers/users.js
+++ controllers/users.js
@@ -5,35 +5,58 @@ const User = require('../models/user');
 
 const getUser = (req, res) => {
   User.find({})
-    .then((data) => { res.status(200).send({ data }); })
+    .then((data) => {
+      res.status(200).send({ data });
+    })
     .catch((err) => {
       const ERROR_CODE = 404;
 
-      if (err.name === 'CastError') { res.status(ERROR_CODE).send({ message: 'Запрашиваемый ресурс не найден' }); } else { res.status(500).send({ message: 'Ошибка на сервере' }); }
+      if (err.name === 'CastError') {
+        res
+          .status(ERROR_CODE)
+          .send({ message: 'Запрашиваемый ресурс не найден' });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
     });
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params._id)
     .orFail(new Error('Not Found'))
-    .then((data) => { res.status(200).send({ data }); })
+    .then((data) => {
+      res.status(200).send({ data });
+    })
     .catch((err) => {
-      const ERROR_CODE = 400;
+      const ERROR_CODE = 404;
 
-      if (err.name === 'CastError' || err.message === 'Not Found') { res.status(ERROR_CODE).send({ message: 'пользователь не найден' }); } else { res.status(500).send({ message: 'Ошибка на сервере' }); }
+      if (err.name === 'CastError' || err.message === 'Not Found') {
+        res.status(ERROR_CODE).send({ message: 'пользователь не найден' });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
     });
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
+
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       const ERROR_CODE = 400;
-
-      if (err.name === 'CastError') { res.status(ERROR_CODE).send({ message: 'переданы некорректные данные в метод создания пользователя' }); } else { res.status(500).send({ message: 'Ошибка на сервере' }); }
+      if (err.name === 'ValidationError') {
+        res
+          .status(ERROR_CODE)
+          .send({
+            message:
+              'переданы некорректные данные в метод создания пользователя',
+          });
+      } else {
+        res.status(500).send({ message: 'Ошибка на сервере' });
+      }
     });
 };
 
